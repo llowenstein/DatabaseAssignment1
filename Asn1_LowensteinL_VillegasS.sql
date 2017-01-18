96820858af9c65b564c916af6aa3e3337b0aa94a
Sergio 'Josh' Villegas <joshviba_7@hotmail.com>
2017-01-18 11:28:39 -0800

    Spool file added

--- Asn1_LowensteinL_VillegasS.sql
+++ Asn1_LowensteinL_VillegasS.sql
@@ -1,17 +1,17 @@
-SET ECHO ON
+SET ECHO ON;
+--
+SPOOL "C:\Users\Sergio\Documents\SchoolWork\COMP 2714\Asgnmnts\Asssn1_GITRepo\Asn1_LowensteinL_VillegasS.txt";
 --
 -- ---------------------------------------------------------
 --  COMP 2714
 --  SET 2D
 --  Assignment Asn01
---  Villegas, Sergio    A00
+--  Villegas, Sergio    A00935797
 --  Lowenstein, Luli    A00946133
---  email: @bcit.ca
---  email: llowenstein@bcit.ca
+--  email: jvillegas4@my.bcit.ca
+--  email: llowenstein@my.bcit.ca
 -- ---------------------------------------------------------
 ALTER SESSION SET NLS_DATE_FORMAT='YYYY-MM-DD';
---
-SPOOL PATH-HERE/Asn1_LowensteinL_VillegasS.txt
 --
 --  Q1. Dropping all tables
 DROP TABLE Booking;
@@ -91,46 +91,50 @@ INSERT INTO Guest
   VALUES ('G004', 'Yashar Nesvaderani', '666 Fail St');
 --  Q4. Inserting sample data into the Booking table.
 INSERT INTO Booking
-  VALUES ('H0003', 'G002', '2016-08-08', '2016-09-08', '056');
+  VALUES ('H0003', 'G002', TO_DATE('2016-08-08'), TO_DATE('2016-09-08'), '056');
 INSERT INTO Booking
-  VALUES ('H0001', 'G004', '2016-10-05', '2016-10-20', '001');
+  VALUES ('H0001', 'G004', TO_DATE('2016-10-05'), TO_DATE('2016-10-20'), '001');
 INSERT INTO Booking
-  VALUES ('H0002', 'G003', '2017-01-03', '2017-01-06', '075');
+  VALUES ('H0002', 'G003', TO_DATE('2017-01-03'), TO_DATE('2017-01-06'), '075');
 INSERT INTO Booking
-  VALUES ('H0004', 'G001', '2016-08-08', '2016-09-10', '100');
---  Q5. Dropping the check constraint for Room table.
+  VALUES ('H0004', 'G001', TO_DATE('2016-08-08'), TO_DATE('2016-09-10'), '100');
+--  Q5a. Dropping the check constraint for Room table.
 ALTER TABLE Room
 DROP CONSTRAINT chk_type;
---  Q5. Modifying the check constraint for Room so it accepts 'Deluxe'.
+--  Q5a. Modifying the check constraint for Room so it accepts 'Deluxe'.
 ALTER TABLE Room
 ADD CONSTRAINT chk_Type
   CHECK (type IN ('Deluxe', 'Single', 'Double', 'Family'));
---  Q5. Adding a new column called Discount. Default 0, and must be between 0 and 30.
+--  Q5b. Adding a new column called Discount. Default 0, and must be between 0 and 30.
 ALTER TABLE Room
   ADD discount  DECIMAL(5)  DEFAULT 0 NOT NULL;
 ALTER TABLE Room
-  ADD CONSTRAINT chk_Discount CHECK (discount BETWEEN 0 AND 30);
---  Q6. Increasing the price of 'Double' rooms in a hotel of our choice by 15%.
+  ADD CONSTRAINT chk_Discount
+  CHECK (discount BETWEEN 0 AND 30);
+--  Q6a. Increasing the price of 'Double' rooms in a hotel of our choice by 15%.
 UPDATE Room
-SET price = 23
-WHERE hotelNo = 'H0002' AND type = 'Double';
---  Q6. Modifying the booking for a guest that arrived early.
+SET price = price * 1.15;
+WHERE hotelNo = 'H0002'
+  AND type = 'Double';
+--  Q6b. Modifying the booking for a guest that arrived early.
 UPDATE Booking
-SET dateFrom = '2016-07-28', dateTo = '2016-10-05'
-WHERE guestNo = 'G001'
---  Q7. Creating archive table for Booking table.
+SET dateFrom = TO_DATE('2016-07-28'), dateTo = TO_DATE('2016-10-05')
+WHERE guestNo = 'G001';
+--  Q7a. Creating archive table for Booking table.
 CREATE TABLE OldBooking
 (hotelNo    CHAR(10)    NOT NULL
 ,guestNo    CHAR(10)    NOT NULL
 ,dateFrom   VARCHAR(10) NOT NULL
 ,dateTo     VARCHAR(10) NOT NULL
 ,roomNo     CHAR(10)    NOT NULL
-,CONSTRAINT PKOldBooking PRIMARY KEY (guestNo)
+,CONSTRAINT PKOldBooking PRIMARY KEY (hotelNo, guestNo, dateFrom, roomNo)
 );
---  Q7. Copying rows in Booking table with dates before 2017-01-01 to OldBooking table.
+--  Q7b. Copying rows in Booking table with dates before 2017-01-01 to OldBooking table.
 INSERT INTO OldBooking
 SELECT * FROM Booking
-WHERE dateTo <= '2017-01-01';
---  Q7. Deleting rows in Booking table that have dates before 2017-01-01.
+WHERE dateTo < TO_DATE('2017-01-01');
+--  Q7c. Deleting rows in Booking table that have dates before 2017-01-01.
 DELETE FROM Booking
-WHERE dateTo <= '2017-01-01';
+WHERE dateTo <= TO_DATE('2017-01-01');
+--
+SPOOL OFF;
